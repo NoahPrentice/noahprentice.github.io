a283bcb2549d37bbb4fce2cf93a392f4aec1dbcb
NoahPrentice <noahkprentice@gmail.com>
2025-01-11 23:24:27 -0800

    Completed draft of CV page, with TOC

--- JS/header.js
+++ JS/header.js
@@ -6,3 +6,26 @@ header.style.backgroundColor = headerColor;
 
 var headerLinks = document.getElementById('header-links-container');
 headerLinks.style.height = headerHeight + "em";
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function dropdownOnClick() {
+    document.getElementById("TOCDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function (event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+
+var dropdownDiv = document.getElementById("dropdownDiv")
+dropdownDiv.style.top = 5.3 + "em";
