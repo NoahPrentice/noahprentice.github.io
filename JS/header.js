34ffb3dfada2b5af3bb95bd9100dd1e20ad727cd
NoahPrentice <noahkprentice@gmail.com>
2025-01-12 10:24:09 -0800

    Icon click fix take 3

--- JS/header.js
+++ JS/header.js
@@ -15,7 +15,7 @@ function dropdownOnClick() {
 
 // Close the dropdown menu if the user clicks outside of it
 window.onclick = function (event) {
-    if (!event.target.matches('.dropbtn')) {
+    if (!event.target.matches('.dropbtn') && !event.target.matches('dropbtnIcon')) {
         var dropdowns = document.getElementsByClassName("dropdown-content");
         var i;
         for (i = 0; i < dropdowns.length; i++) {
